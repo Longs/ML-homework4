10a33aa4c5fe815459b551a1cac42c03b7f5b103
Michael Stoner <michael_stoner@hotmail.co.uk>
2021-06-19 15:42:32 +0100

    7.2 complete

--- hw4q6.py
+++ hw4q6.py
@@ -162,3 +162,95 @@ print(ans)"""
 
 ans=svm_obj(x_1, y_1, 0.1*th1, th1_0, 0.0)
 print(ans)
+
+#########################################
+# Support Vector Machine gradient
+#########################################
+
+# Returns the gradient of hinge(v) with respect to v.
+def d_hinge(v):
+    return np.where (v<1,-1,0)
+
+# Returns the gradient of hinge_loss(x, y, th, th0) with respect to th
+#This will be 0 (if v>1) or x elements otherwise
+def d_hinge_loss_th(x, y, th, th0):
+    out=x.copy()
+    v = (np.dot(x.T,th)+th0)
+    for i in range(len(v)):
+        raw = v[i][0]*y[0][i]
+        if raw > 1:
+            for _ in out:
+                _[i] = 0
+
+    return out
+    
+
+# Returns the gradient of hinge_loss(x, y, th, th0) with respect to th0
+def d_hinge_loss_th0(x, y, th, th0):
+    out=[]
+    v = (np.dot(x.T,th)+th0)
+    for i in range(len(v)):
+        raw = v[i][0]*y[0][i]
+        out.append(np.where(raw<1,1,0))
+
+    return np.array(out)
+
+# Returns the gradient of svm_obj(x, y, th, th0) with respect to th
+# which is the average of the individual gradients +  d/dth of norm squared
+def d_svm_obj_th(x, y, th, th0, lam):
+    out=[]
+    raw = d_hinge_loss_th(x, y, th, th0)
+    for _ in range(len(raw)):
+        out.append(np.average(raw[_])+ 2*lam*th[_])
+    return np.array(out)
+
+# Returns the gradient of svm_obj(x, y, th, th0) with respect to th0
+# which is the average of the individual gradients
+def d_svm_obj_th0(x, y, th, th0, lam):
+    raw = d_hinge_loss_th0(x, y, th, th0)
+    out=[]
+    out.append(np.average(raw))
+    return np.array(out)
+
+# Returns the full gradient as a single vector (which includes both th, th0)
+def svm_obj_grad(x, y, th, th0, lam):
+    out = d_svm_obj_th(x, y, th, th0, lam).tolist()
+    out.append(d_svm_obj_th0(x, y, th, th0, lam))
+
+    return np.array(out)
+
+# Returns the gradient of hinge(v) with respect to v.
+def d_hinge(v):
+    return np.where (v<1,-1,0)
+    
+
+
+
+X1 = np.array([[1, 2, 3, 9, 10]])
+y1 = np.array([[1, 1, 1, -1, -1]])
+th1, th10 = np.array([[-0.31202807]]), np.array([[1.834     ]])
+X2 = np.array([[2, 3, 9, 12],
+               [5, 2, 6, 5]])
+y2 = np.array([[1, -1, 1, -1]])
+th2, th20=np.array([[ -3.,  15.]]).T, np.array([[ 2.]])
+
+print(d_hinge(np.array([[ 71.]])).tolist())
+print(d_hinge(np.array([[ -23.]])).tolist())
+print(d_hinge(np.array([[ 71, -23.]])).tolist())
+
+print("hl th")
+print(d_hinge_loss_th(X2[:,0:1], y2[:,0:1], th2, th20).tolist())
+print(d_hinge_loss_th(X2, y2, th2, th20).tolist())
+print("hl th0")
+print(d_hinge_loss_th0(X2[:,0:1], y2[:,0:1], th2, th20).tolist())
+print(d_hinge_loss_th0(X2, y2, th2, th20).tolist())
+
+print("svmth")
+print(d_svm_obj_th(X2[:,0:1], y2[:,0:1], th2, th20, 0.01).tolist())
+print(d_svm_obj_th(X2, y2, th2, th20, 0.01).tolist())
+print("svmth0")
+print(d_svm_obj_th0(X2[:,0:1], y2[:,0:1], th2, th20, 0.01).tolist())
+print(d_svm_obj_th0(X2, y2, th2, th20, 0.01).tolist())
+print("objgrad")
+print(svm_obj_grad(X2, y2, th2, th20, 0.01).tolist())
+print(svm_obj_grad(X2[:,0:1], y2[:,0:1], th2, th20, 0.01).tolist())
